e4cc2abe60747df22394bac39014f24dd1d6c7e1
Michael Pauly <mpauly02@gmail.com>
2015-06-08 19:22:44 -0500

    First commit

---
+++ app/src/main/java/com/example/michaelpauly/taskit/Task.java
@@ -0,0 +1,40 @@
+package com.example.michaelpauly.taskit;
+
+import java.util.Date;
+
+/**
+ * Created by Michael Pauly on 6/8/2015.
+ */
+public class Task {
+    private String mName;
+    private Date mDueDate;
+    private boolean mDone;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public Date getDueDate() {
+        return mDueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        mDueDate = dueDate;
+    }
+
+    public boolean isDone() {
+        return mDone;
+    }
+
+    public void setDone(boolean done) {
+        mDone = done;
+    }
+
+    public String toString() {
+        return mName;
+    }
+}
---
+++ app/src/main/java/com/example/michaelpauly/taskit/TaskListActivity.java
@@ -0,0 +1,69 @@
+package com.example.michaelpauly.taskit;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class TaskListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+
+        Task[] items = new Task[3];
+        items[0] = new Task();
+        items[1] = new Task();
+        items[2] = new Task();
+        items[0].setName("Task 1");
+        items[1].setName("Task 2");
+        items[2].setName("Task 3");
+
+        ListView listView = (ListView)findViewById(R.id.task_list);
+        listView.setAdapter(new TaskAdapter(items));
+    }
+
+    private class TaskAdapter extends ArrayAdapter<Task> {
+        TaskAdapter(Task[] tasks){
+            super(TaskListActivity.this, R.layout.task_list_row, R.id.task_item_name , tasks);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+
+            return convertView;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_task_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
