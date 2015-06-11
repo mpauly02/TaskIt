ec153137bc1e45bacfff1f25939e8e3b92f3a4e7
Michael Pauly <mpauly02@gmail.com>
2015-06-10 19:32:20 -0500

    Finished first part of the app, can actually edit the tasks now. This also implements Date Picker as well. Also added an Add Task menu item (this menu item does not do anything yet).

--- app/src/main/java/com/example/michaelpauly/taskit/TaskActivity.java
+++ app/src/main/java/com/example/michaelpauly/taskit/TaskActivity.java
@@ -1,56 +1,94 @@
 package com.example.michaelpauly.taskit;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
-public class TaskActivity extends Activity {
+public class TaskActivity extends AppCompatActivity {
 
     public static final String EXTRA = "TaskExtra";
+
+    private Calendar mCal;
+    private Task mTask;
+    private Button mDateButton;
+    private EditText mTaskNameInput;
+    private CheckBox mDoneBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
 
-        Task task = (Task)getIntent().getSerializableExtra(EXTRA);
+        mTask = (Task)getIntent().getSerializableExtra(EXTRA);
 
-        EditText taskNameInput = (EditText)findViewById(R.id.task_name);
-        Button dateButton = (Button)findViewById(R.id.task_date);
-        CheckBox doneBox = (CheckBox)findViewById(R.id.task_done);
+        mCal = Calendar.getInstance();
+
+        mTaskNameInput = (EditText)findViewById(R.id.task_name);
+        mDateButton = (Button)findViewById(R.id.task_date);
+        mDoneBox = (CheckBox)findViewById(R.id.task_done);
         Button saveButton = (Button)findViewById(R.id.save_button);
 
-        taskNameInput.setText(task.getName());
-        if (task.getDueDate() == null) {
-            dateButton.setText(getResources().getString(R.string.no_date));
-        }else {
-            DateFormat df = DateFormat.getDateInstance();
-            dateButton.setText(df.format(task.getDueDate()));
+        mTaskNameInput.setText(mTask.getName());
+        if (mTask.getDueDate() == null) {
+            mCal.setTime(new Date());
+            mDateButton.setText(getResources().getString(R.string.no_date));
+        } else{
+            mCal.setTime(mTask.getDueDate());
+            updateButton();
         }
-        doneBox.setChecked(task.isDone());
+        mDoneBox.setChecked(mTask.isDone());
 
-        dateButton.setOnClickListener(new View.OnClickListener() {
+        mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                DatePickerDialog dpd = new DatePickerDialog(TaskActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        mCal.set(Calendar.YEAR, year);
+                        mCal.set(Calendar.MONTH, monthOfYear);
+                        mCal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 
+                        updateButton();
+                    }
+                }, mCal.get(Calendar.YEAR), mCal.get(Calendar.MONTH), mCal.get(Calendar.DAY_OF_MONTH));
+
+                dpd.show();
             }
         });
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mTask.setName(mTaskNameInput.getText().toString());
+                mTask.setDone(mDoneBox.isChecked());
+                mTask.setDueDate(mCal.getTime());
 
+                Intent i = new Intent();
+                i.putExtra(EXTRA, mTask);
+                setResult(RESULT_OK, i);
+                finish();
             }
         });
+    }
+
+    private void updateButton() {
+        DateFormat df = DateFormat.getDateInstance();
+        mDateButton.setText(df.format(mCal.getTime()));
     }
 
     @Override
--- app/src/main/java/com/example/michaelpauly/taskit/TaskListActivity.java
+++ app/src/main/java/com/example/michaelpauly/taskit/TaskListActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,38 +16,60 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 
-public class TaskListActivity extends Activity {
+public class TaskListActivity extends AppCompatActivity {
+
+    private static final int EDIT_TASK_REQUEST = 10;
+
+    private ArrayList<Task> mTasks;
+    private int mLastPositionClicked;
+    private TaskAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
 
-        Task[] items = new Task[3];
-        items[0] = new Task();
-        items[1] = new Task();
-        items[2] = new Task();
-        items[0].setName("Task 1");
-        items[1].setName("Task 2");
-        items[2].setName("Task 3");
+        mTasks = new ArrayList<Task>();
+        mTasks.add(new Task());
+        mTasks.add(new Task());
+        mTasks.add(new Task());
+        mTasks.get(0).setDueDate(new Date());
+        mTasks.get(0).setName("Task 1");
+        mTasks.get(1).setName("Task 2");
+        mTasks.get(2).setName("Task 3");
 
         ListView listView = (ListView)findViewById(R.id.task_list);
-        listView.setAdapter(new TaskAdapter(items));
+        mAdapter = new TaskAdapter(mTasks);
+        listView.setAdapter(mAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mLastPositionClicked = position;
                 Intent i = new Intent(TaskListActivity.this, TaskActivity.class);
                 Task task = (Task)parent.getAdapter().getItem(position);
                 i.putExtra(TaskActivity.EXTRA, task);
-                startActivity(i);
+                startActivityForResult(i, EDIT_TASK_REQUEST);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == EDIT_TASK_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Task task = (Task)data.getSerializableExtra(TaskActivity.EXTRA);
+                mTasks.set(mLastPositionClicked, task);
+                mAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+
     private class TaskAdapter extends ArrayAdapter<Task> {
-        TaskAdapter(Task[] tasks){
+        TaskAdapter(ArrayList<Task> tasks){
             super(TaskListActivity.this, R.layout.task_list_row, R.id.task_item_name , tasks);
         }
 
